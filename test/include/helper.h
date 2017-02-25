f52bae98063514d3690c8c945aa6ea64bfebd111
Zhuhao Wang <zhuhaow@gmail.com>
2017-02-25 12:06:13 +0800

    Refine test for tcp connection

--- test/include/helper.h
+++ test/include/helper.h
@@ -4,7 +4,9 @@
 #include "check.h"
 #include "mem.h"
 #include "uv.h"
+#include "error.h"
 #include <assert.h>
+#include <string.h>
 
 #define TEST_PORT 9123
 
@@ -19,18 +21,38 @@
     return number_failed;                                                      \
   }
 
+typedef struct {
+  int socket_accept;
+  int bytes_read;
+  int bytes_write;
+  int read_count;
+  int write_count;
+  int alloc_count;
+  ne_error_code last_error;
+} socket_stat;
+
+typedef struct {
+  uv_tcp_t handle;
+  socket_stat listen_stat;
+  socket_stat socket_stat;
+} server_t;
+
 void server_write_cb(uv_write_t *req, int status);
 
-void start_server(uv_tcp_t *server, uv_connection_cb conn_cb) {
-  assert(uv_tcp_init(uv_default_loop(), server) == 0);
+void start_server(server_t *server, uv_connection_cb conn_cb) {
+  memset(server, 0, sizeof(server_t));
+  assert(uv_tcp_init(uv_default_loop(), &server->handle) == 0);
+  server->handle.data = server;
   struct sockaddr_in addr;
   assert(uv_ip4_addr("0.0.0.0", TEST_PORT, &addr) == 0);
-  assert(uv_tcp_bind(server, (struct sockaddr *)&addr, 0) == 0);
-  assert(uv_listen((uv_stream_t *)server, 30, conn_cb) == 0);
+  assert(uv_tcp_bind(&server->handle, (struct sockaddr *)&addr, 0) == 0);
+  assert(uv_listen((uv_stream_t *)&server->handle, 30, conn_cb) == 0);
 }
 
 void vanilla_alloc_cb(uv_handle_t *handle, size_t suggested_size,
                      uv_buf_t *buf) {
+  server_t *server = (server_t *)handle->data;
+  server->socket_stat.alloc_count++;
   buf->base = malloc(suggested_size);
   buf->len = suggested_size;
 }
@@ -47,12 +69,16 @@ void free_buf(const uv_buf_t *buf) {
 }
 
 void server_read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
+  server_t *server = (server_t *)stream->data;
   if (nread < 0) {
     free_buf(buf);
-    printf("Closed socket due to error %ld", nread);
+    server->socket_stat.last_error = nread;
     uv_close((uv_handle_t *)stream, close_free_cb);
     return;
   }
+
+  server->socket_stat.read_count++;
+  server->socket_stat.bytes_read += nread;
 
   if (nread == 0) {
     free_buf(buf);
@@ -62,15 +88,27 @@ void server_read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
   assert(uv_read_stop(stream) == 0);
   uv_write_t *req = ALLOC(uv_write_t, 1);
   assert(req);
-  req->data = buf->base;
-  uv_write(req, stream, buf, 1, server_write_cb);
+  uv_buf_t *write_buf = ALLOC(uv_buf_t, 1);
+  write_buf->base = buf->base;
+  write_buf->len = buf->len;
+  req->data = write_buf;
+  uv_write(req, stream, write_buf, 1, server_write_cb);
 }
 
 void server_write_cb(uv_write_t *req, int status) {
+  server_t *server = (server_t *)req->handle->data;
+
+  server->socket_stat.write_count++;
+
   assert(status == 0);
-  uv_stream_t *server_sock = req->handle;
-  free(req->data);
+
+  uv_buf_t *write_buf = (uv_buf_t *)req->data;
+  server->socket_stat.bytes_write += write_buf->len;
+  free(write_buf->base);
+  free(write_buf);
   free(req);
+
+  uv_stream_t *server_sock = req->handle;
   uv_read_start(server_sock, vanilla_alloc_cb, server_read_cb);
 }
 
@@ -80,8 +118,10 @@ void echo_server_conn_cb(uv_stream_t *server, int status) {
   uv_tcp_t *server_sock = ALLOC(uv_tcp_t, 1);
   assert(server_sock);
   assert(uv_tcp_init(uv_default_loop(), server_sock) == 0);
+  server_sock->data = server->data;
 
   assert(uv_accept(server, (uv_stream_t *)server_sock) == 0);
+  ((server_t *)server->data)->listen_stat.socket_accept++;
   uv_read_start((uv_stream_t *)server_sock, vanilla_alloc_cb, server_read_cb);
 }
 
--- test/socket/test_tcp_socket.c
+++ test/socket/test_tcp_socket.c
@@ -12,8 +12,8 @@ void on_connect1(ne_tcp_socket_t *socket) {
   uv_close(socket->context, NULL);
 }
 
-START_TEST (tcp_socket_test) {
-  uv_tcp_t server;
+START_TEST (connect_test) {
+  server_t server;
   start_server(&server, echo_server_conn_cb);
 
   ne_tcp_socket_t socket;
@@ -29,6 +29,16 @@ START_TEST (tcp_socket_test) {
   uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 
   ck_assert(uv_loop_close(uv_default_loop()) == 0);
+
+  ck_assert_int_eq(server.listen_stat.socket_accept, 1);
+  ck_assert_int_eq(server.listen_stat.last_error, 0);
+
+  ck_assert_int_eq(server.socket_stat.last_error, UV_EOF);
+  ck_assert_int_le(server.socket_stat.alloc_count, 1);
+  ck_assert_int_eq(server.socket_stat.bytes_read, 0);
+  ck_assert_int_eq(server.socket_stat.read_count, 0);
+  ck_assert_int_eq(server.socket_stat.bytes_write, 0);
+  ck_assert_int_eq(server.socket_stat.write_count, 0);
 }
 END_TEST
 
@@ -38,8 +48,8 @@ Suite *build_suite() {
 
   s = suite_create("TCP Socket");
 
-  tcp_case = tcase_create("Socket");
-  tcase_add_test(tcp_case, tcp_socket_test);
+  tcp_case = tcase_create("Socket connect");
+  tcase_add_test(tcp_case, connect_test);
 
   suite_add_tcase(s, tcp_case);
 
