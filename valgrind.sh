63c9f8e17826345dd1d0f93e55bae0cb0b7c0fbd
Zhuhao Wang <zhuhaow@gmail.com>
2017-02-26 15:29:04 +0800

    Update docker test script

--- valgrind.sh
+++ valgrind.sh
@@ -4,6 +4,7 @@ docker run --rm -v ${PWD}:/app -w /app zhuhaow/libnekit-test sh -c "
 mkdir /libnekit && \
 cd /libnekit && \
 cp -r /app/* . && \
+./scripts/build_deps.sh && \
 rm -rf build && \
 mkdir build && \
 cd build && \
