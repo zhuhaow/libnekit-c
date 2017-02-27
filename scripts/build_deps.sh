ce32fc6a8d038a2ed45f5a4011fb35c5de475a1d
Zhuhao Wang <zhuhaow@gmail.com>
2017-02-27 22:52:42 +0800

    Test build for both 32 and 64 bit on mac and linux

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -7,12 +7,34 @@ SCRIPTPATH=`pwd`
 popd > /dev/null
 
 LIBUV_PATH=$SCRIPTPATH/../deps/libuv/
-
 cd $LIBUV_PATH
-sh autogen.sh
-./configure --prefix=`pwd`/../deps_build/
-make
-make install
+
+git clone https://chromium.googlesource.com/external/gyp.git build/gyp
+
+mkdir -p ../deps_build/lib
+mkdir -p ../deps_build/include
+cp -r include/* ../deps_build/include/
+
+if [ "$(uname)" == "Darwin" ]; then
+    ./gyp_uv.py -f xcode
+    if [ "$ARCH" == "x86" ]; then
+        xcodebuild -ARCHS="i386" -project uv.xcodeproj \
+                   -configuration Release -target All
+    else
+        xcodebuild -ARCHS="x86_64" -project uv.xcodeproj \
+                   -configuration Release -target All
+    fi
+    pwd
+    cp build/Release/libuv.a ../deps_build/lib/
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    if [ "$ARCH" == "x86" ]; then
+        ./gyp_uv.py -f make -Dtarget_arch=x32
+    else
+        ./gyp_uv.py -f make
+    fi
+    BUILDTYPE=Release make -C out
+    cp out/Release/libuv.a ../deps_build/lib/
+fi
 
 cd $CURRENTPATH
 
