c056b610f99df5425ffc25b9c045004b052d03e7
Zhuhao Wang <zhuhaow@gmail.com>
2017-03-05 20:14:11 +0800

    Add UNUSED macro to silence some warnings

--- include/config.h
+++ include/config.h
@@ -5,4 +5,11 @@
 
 #define SOCKET_TIMEOUT 300
 
+#ifdef __GNUC__
+#  define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+#else
+#  define UNUSED(x) UNUSED_ ## x
+#endif
+
+
 #endif /* CONFIG_H */
